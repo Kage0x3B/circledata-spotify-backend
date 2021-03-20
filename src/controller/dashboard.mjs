a52c2bc3360eb95e8a9e88ae53de233226fd441c
Kage0x3B <moritz.hein@live.de>
2021-03-20 20:00:52 +0100

    Current device list route

--- src/controller/dashboard.mjs
+++ src/controller/dashboard.mjs
@@ -6,19 +6,38 @@ const router = express.Router();
 
 router.get("/", ensureAuthenticated, (req, res) => {});
 
-router.get("/currentlyPlaying", ensureAuthenticated, async (req, res) => {
+router.get("/currentlyPlaying", ensureAuthenticated, async (req, res, next) => {
     const spotifyApi = getSpotifyApiForUser(req.user);
 
     try {
         const data = await doSpotifyRequest(spotifyApi, spotifyApi.getMyCurrentPlayingTrack);
 
         if (data.context && data.context.type === "playlist") {
-            data.playlist = await doSpotifyRequest(spotifyApi, spotifyApi.getPlaylist, data.context.uri.split(":")[2]);
+            const contextUriSplit = data.context.uri.split(":");
+            try {
+                data.playlist = await doSpotifyRequest(
+                    spotifyApi,
+                    spotifyApi.getPlaylist,
+                    contextUriSplit[contextUriSplit.length - 1]
+                );
+            } catch (ignored) {}
         }
 
         res.json(data);
     } catch (err) {
-        console.log(err);
+        next(err);
+    }
+});
+
+router.get("/currentDevices", ensureAuthenticated, async (req, res, next) => {
+    const spotifyApi = getSpotifyApiForUser(req.user);
+
+    try {
+        const data = await doSpotifyRequest(spotifyApi, spotifyApi.getMyDevices);
+
+        res.json(data);
+    } catch (err) {
+        next(err);
     }
 });
 
